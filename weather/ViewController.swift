bf8402a1d025823f8428d6fcbc6d35124cd4ffd6
Justin Stinson <justin.stinson@garmin.com>
2015-08-10 18:56:34 -0400

    Add new data to UI and clean up loading animation
    Add additional weather data (description, temperature, wind speed,
    humidity, and pressure) to the UI. Also clean up the loading
    transition with a fade in/out of all data fields.

--- weather/ViewController.swift
+++ weather/ViewController.swift
@@ -14,15 +14,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var weatherIconImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var refreshButton: UIBarButtonItem!
+    @IBOutlet weak var dataView: UIView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var detailsTableView: UITableView!
 
-    let weatherManager: WeatherManager! = WeatherManager()
-    var currentWeather: CurrentWeather?
+    private let weatherManager: WeatherManager! = WeatherManager()
+    private var currentWeather: CurrentWeather?
+    private var currentWeatherImage: UIImage?
+    private var currentWeatherDetails = [(String, String)]()
+    private var isLoading: Bool = false
 
-    var locationAuthorizationStatus: CLAuthorizationStatus = .NotDetermined
-    let locationManager: CLLocationManager! = CLLocationManager()
-    var location: CLLocation?
-    var isLoading: Bool = false
+    private var locationAuthorizationStatus: CLAuthorizationStatus = .NotDetermined
+    private let locationManager: CLLocationManager! = CLLocationManager()
+    private var location: CLLocation?
 
     private static let locationChangeThreshold: CLLocationDistance = 10.0
 
@@ -88,7 +94,7 @@ class ViewController: UIViewController {
         self.activityIndicator.startAnimating()
         UIView.animateWithDuration(0.25) { () -> Void in
             self.statusLabel.alpha = 1.0
-            self.weatherIconImageView.alpha = 0.0
+            self.dataView.alpha = 0.0
         }
     }
 
@@ -102,23 +108,65 @@ class ViewController: UIViewController {
         self.activityIndicator.stopAnimating()
         UIView.animateWithDuration(0.25) { () -> Void in
             self.statusLabel.alpha = 0.0
-            self.weatherIconImageView.alpha = 1.0
+            self.dataView.alpha = 1.0
         }
     }
 
-    private func updateWeatherIcon(image: UIImage) {
-        let hideAnimationClosure = { () -> Void in
-            self.weatherIconImageView.alpha = 0.0
+    private func updateWeatherDataDisplay() {
+        if let temperature = self.currentWeather?.main.temp {
+            let temperatureString = "\(temperature) °F"
+            self.temperatureLabel.text = temperatureString
         }
-        UIView.animateWithDuration(0.5, animations: hideAnimationClosure) {
-            (_: Bool) -> Void in
-            self.weatherIconImageView.image = image
-            UIView.animateWithDuration(0.5, animations: { () -> Void in
-                self.weatherIconImageView.alpha = 1.0
-            })
+
+        if let description = self.currentWeather?.weather.main {
+            var descriptionString = description
+            if let descriptionDetails = self.currentWeather?.weather.conditionDescription {
+                descriptionString += " (\(descriptionDetails))"
+            }
+            self.descriptionLabel.text = descriptionString
         }
+
+        self.currentWeatherDetails.removeAll(keepCapacity: true)
+        if let windSpeed = self.currentWeather?.wind.speed {
+            let item = ("Wind", "\(windSpeed) mph")
+            self.currentWeatherDetails.append(item)
+        }
+
+        if let humidity = self.currentWeather?.main.humidity {
+            let item = ("Humidity", "\(humidity) %")
+            self.currentWeatherDetails.append(item)
+        }
+
+        if let pressure = self.currentWeather?.main.pressure {
+            let item = ("Pressure", "\(pressure) hPa")
+            self.currentWeatherDetails.append(item)
+        }
+
+        self.detailsTableView.reloadData()
     }
 
+    private func updateWeatherIcon() {
+        self.weatherIconImageView.image = self.currentWeatherImage
+    }
+
+}
+
+extension ViewController: UITableViewDataSource {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.currentWeatherDetails.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("default", forIndexPath: indexPath)
+        let detailItem = self.currentWeatherDetails[indexPath.row]
+        cell.textLabel?.text = detailItem.0
+        cell.detailTextLabel?.text = detailItem.1
+        return cell
+    }
 }
 
 extension ViewController: CLLocationManagerDelegate {
@@ -162,11 +210,17 @@ extension ViewController: CLLocationManagerDelegate {
             (currentWeather: CurrentWeather) in
             self.currentWeather = currentWeather
 
+            dispatch_async(dispatch_get_main_queue(), {
+                self.updateWeatherDataDisplay()
+            })
+
             self.weatherManager.requestIconForWeather(self.currentWeather!, completion: {
                 (image: UIImage?) -> Void in
                 if let image = image {
+                    self.currentWeatherImage = image
+
                     dispatch_async(dispatch_get_main_queue(), {
-                        self.updateWeatherIcon(image)
+                        self.updateWeatherIcon()
                         self.stopLoading()
                     })
                 }
@@ -182,7 +236,7 @@ extension ViewController: CLLocationManagerDelegate {
         let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
         alertController.addAction(okAction)
         self.presentViewController(alertController, animated: true, completion: nil)
-
     }
+
 }
 
